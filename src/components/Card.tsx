c4ac8698afaaafc2e57ae0c9241a2e0ca12744fc
dballard10 <dylanballard55@gmail.com>
2025-02-28 20:48:57 -0500

    Make image link to show details. Add search bar component.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,11 +13,13 @@ interface CardProps {
 const Card = ({ id = 0, title, description, image, rating }: CardProps) => {
   return (
     <div className="bg-white/10 backdrop-blur-lg border border-white/20 rounded-lg hover:scale-105 transition-all duration-300 overflow-hidden">
-      <img
-        src={image}
-        alt={title}
-        className="w-full h-48 object-cover rounded-t-lg"
-      />
+      <a href={`/shows/${id}`}>
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-48 object-cover rounded-t-lg"
+        />
+      </a>
       <div className="p-6">
         <div className="flex flex-row items-center justify-between mb-2">
           <div className="flex flex-row gap-3">
--- src/components/GridDisplay.tsx
+++ src/components/GridDisplay.tsx
@@ -2,6 +2,7 @@ import AllShowsGrid from "./AllShowsGrid";
 import CurrentShowsGrid from "./CurrentShowsGrid";
 import FavoritesGrid from "./FavoritesGrid";
 import Platforms from "./Platforms";
+import SearchBar from "./SearchBar";
 import Sort from "./Sort";
 
 interface GridDisplayProps {
@@ -17,15 +18,16 @@ const GridDisplay = ({
 }: GridDisplayProps) => {
   return (
     <>
+      <SearchBar />
+      <div className="flex flex-row gap-4">
+        <Platforms />
+        <Sort />
+      </div>
       {/* Current Watches */}
       <h1 className="text-3xl font-bold tracking-tight text-white pb-2">
         {currentShowsText}
       </h1>
       <div className="flex flex-col gap-4">
-        <div className="flex flex-row gap-4">
-          <Platforms />
-          <Sort />
-        </div>
         <CurrentShowsGrid />
       </div>
 
