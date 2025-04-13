a79bcdd0dbd7bb50608462ba88b92549078129c2
dballard10 <dylanballard55@gmail.com>
2025-04-13 01:20:43 -0400

    Revamped the card components. Moved things around. Shows titles when hovered and added the watched button.

--- src/components/card-components/Card.tsx
+++ src/components/card-components/Card.tsx
@@ -5,6 +5,7 @@ import CardImage from "./CardImage";
 import useMediaStore from "@/hooks/use-media-store";
 import MediaType from "./MediaType";
 import useMediaDetails from "@/hooks/media-details/use-media-details";
+import WatchedButton from "./WatchedButton";
 
 interface CardProps {
   id: number;
@@ -35,7 +36,7 @@ const Card = ({
   };
 
   return (
-    <div className="bg-white/10 backdrop-blur-lg border border-white/20 rounded-lg hover:scale-105 transition-all duration-300 overflow-hidden">
+    <div className="relative group bg-white/10 backdrop-blur-lg border border-white/20 rounded-lg hover:scale-105 transition-all duration-300 overflow-hidden">
       <CardImage
         backdrop_path={backdrop_path}
         name={name}
@@ -43,21 +44,26 @@ const Card = ({
         media_type={media_type}
         handleCardClick={handleCardClick}
       />
-      <div className="p-2">
-        <div className="flex flex-row justify-between mb-2">
-          <div className="flex flex-col gap-1">
-            <MediaTitle
-              name={name}
-              id={id}
-              media_type={media_type}
-              handleCardClick={handleCardClick}
-            />
-            <div className="flex flex-row gap-4">
-              <MediaType media_type={media_type} />
+      <div className="absolute top-2 left-2 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <WatchedButton showId={id} />
+      </div>
+      <div className="absolute top-2 right-2 z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <FavoritesButton showId={id} />
+      </div>
+      <div className="absolute inset-0 flex items-end opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <div className="w-full p-2 bg-black bg-opacity-80">
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col gap-1">
+              <MediaTitle
+                name={name}
+                id={id}
+                media_type={media_type}
+                handleCardClick={handleCardClick}
+              />
+              <div className="flex flex-row gap-4">
+                <MediaType media_type={media_type} />
+              </div>
             </div>
-          </div>
-          <div className="flex flex-row gap-1">
-            <FavoritesButton showId={id} />
           </div>
         </div>
       </div>
